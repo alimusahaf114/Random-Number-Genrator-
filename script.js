a52f1ca0f498b62079714900ec8f8c7664a8c3e0
alimusahaf114 <musahafali43@gmail.com>
2024-07-03 07:57:40 -0700

    This is Random Number Generator Program

---
+++ script.js
@@ -0,0 +1,29 @@
+const minNum = 50;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while (running) {
+  guess = window.prompt(`Guess the number between ${minNum} and ${maxNum}`);
+  guess = Number(guess);
+  if (isNaN(guess)) {
+    window.alert("Please enter the valid Number...");
+  } else if (guess < minNum || guess > maxNum) {
+    window.alert("Please enter the valid Number!");
+  } else {
+    attempts++;
+    if (guess < answer) {
+      window.alert("Too Low! try Again  ");
+    } else if (guess > answer) {
+      window.alert("Too High! Try Again.");
+    } else {
+      window.alert(
+        `Correct! The answer was ${answer} you tried ${attempts} attempts`
+      );
+      running = false;
+    }
+  }
+}
